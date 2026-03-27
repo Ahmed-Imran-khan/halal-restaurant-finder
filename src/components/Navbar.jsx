da28d97c64739dcc527c816e49f0d50bb37fdd96
Ahmed Imran <ahmedimrankhaan@gmail.com>
2026-03-27 22:23:54 +0500

    Css fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,38 +10,39 @@ function Navbar({
 }) {
   return (
     <div className="navbar">
-      <div className="navbar-brand">Halal Finder Finland</div>
+      <div className="navbar-brand">Halal Restaurant Finder Finland</div>
+      <div>
+        <input
+          type="text"
+          placeholder="🔍 Search restaurant or city..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
 
-      <input
-        type="text"
-        placeholder="🔍 Search restaurant or city..."
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-      />
+        <select
+          value={cityFilter}
+          onChange={(e) => setCityFilter(e.target.value)}
+        >
+          <option value="">All Cities</option>
+          {cities.map((c, i) => (
+            <option key={i} value={c}>
+              {c}
+            </option>
+          ))}
+        </select>
 
-      <select
-        value={cityFilter}
-        onChange={(e) => setCityFilter(e.target.value)}
-      >
-        <option value="">All Cities</option>
-        {cities.map((c, i) => (
-          <option key={i} value={c}>
-            {c}
-          </option>
-        ))}
-      </select>
-
-      <select
-        value={cuisineFilter}
-        onChange={(e) => setCuisineFilter(e.target.value)}
-      >
-        <option value="">All Cuisines</option>
-        {cuisines.map((c, i) => (
-          <option key={i} value={c}>
-            {c}
-          </option>
-        ))}
-      </select>
+        <select
+          value={cuisineFilter}
+          onChange={(e) => setCuisineFilter(e.target.value)}
+        >
+          <option value="">All Cuisines</option>
+          {cuisines.map((c, i) => (
+            <option key={i} value={c}>
+              {c}
+            </option>
+          ))}
+        </select>
+      </div>
     </div>
   );
 }
--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -31,22 +31,29 @@ function RestaurantCard({ restaurant, onClose }) {
         </div>
 
         <h2 className="card-title">{restaurant.name}</h2>
-        <span className="badge badge-cuisine"><ForkIcons/> {restaurant.cuisine}</span>
+        <span className="badge badge-cuisine">
+          <ForkIcons /> {restaurant.cuisine}
+        </span>
 
-        <p className="card-address">
-          📍 {restaurant.address}, {restaurant.city}
-        </p>
+        <div className="address-body">
+          <p className="card-address">
+            <h4>Location</h4>
+            {restaurant.address}, {restaurant.city}
+            {restaurant.phone && <p>📞 {restaurant.phone}</p>}
+          </p>
 
-        <div className="card-details">
-          {restaurant.phone && <p>📞 {restaurant.phone}</p>}
-          {restaurant.hours && <p>🕐 {restaurant.hours}</p>}
-          {restaurant.website && (
-            <p>
-              <a href={restaurant.website} target="_blank" rel="noreferrer">
-                🌐 Visit Website
-              </a>
-            </p>
-          )}
+          <div className="card-details">
+            <h4>Opening hours</h4>
+            {/* {restaurant.phone && <p>📞 {restaurant.phone}</p>} */}
+            {restaurant.hours && <p>🕐 {restaurant.hours}</p>}
+            {restaurant.website && (
+              <p>
+                <a href={restaurant.website} target="_blank" rel="noreferrer">
+                  🌐 Visit Website
+                </a>
+              </p>
+            )}
+          </div>
         </div>
       </div>
     </div>
